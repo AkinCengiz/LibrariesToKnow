f8d73ad0b1210a85830bc71c9196086c25e71501
akincengiz <akincengiz@outlook.com.tr>
2024-05-18 22:01:29 +0300

    Custom validator added

--- FluentValidationApp.MVC/FluentValidators/CustomerValidator.cs
+++ FluentValidationApp.MVC/FluentValidators/CustomerValidator.cs
@@ -5,11 +5,15 @@ namespace FluentValidationApp.MVC.FluentValidators;
 
 public class CustomerValidator : AbstractValidator<Customer>
 {
-    public string NotEmptyMessage = "{PropertyName} alanı boş geçilemez...";
+    public string NotEmptyMessage { get; } = "{PropertyName} alanı boş geçilemez...";
     public CustomerValidator()
     {
         RuleFor(x => x.Name).NotEmpty().WithMessage(NotEmptyMessage);
         RuleFor(x => x.Email).NotEmpty().WithMessage(NotEmptyMessage).EmailAddress().WithMessage("Email formatında bir değer girmelisiniz...");
         RuleFor(x => x.Age).NotEmpty().WithMessage(NotEmptyMessage).InclusiveBetween(18, 60).WithMessage("Yaş değeri 18 ile 60 arasında bir değer almalıdır...");
+        RuleFor(x => x.BirthDate).NotEmpty().WithMessage(NotEmptyMessage).Must(x =>
+        {
+            return DateTime.Now.AddYears(-18) >= x;
+        }).WithMessage("Yaşınız 18 yaşından büyük olmalıdır...");
     }
 }
--- FluentValidationApp.MVC/Models/Customer.cs
+++ FluentValidationApp.MVC/Models/Customer.cs
@@ -6,4 +6,5 @@ public class Customer
     public string Name { get; set; }
     public string Email { get; set; }
     public int Age { get; set; }
+    public DateTime? BirthDate { get; set; }
 }
